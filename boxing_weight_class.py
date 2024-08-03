3eceaa0d88094eb67fd09b891dd541bbc6b3247c
Quocviet Le <quocvietlet@gmail.com>
2024-08-02 22:26:43 -0500

    list for weightclasses

--- boxing_weight_class.py
+++ boxing_weight_class.py
@@ -1,74 +1,47 @@
+def get_gender():
+    while True:
+        x = input("Are you a man or woman: ")
+        if x in ["man", "women"]:
+            return x
+        else:
+            print("Invalid input. Please Type 'man' or 'woman'")
+
 
 def Boxing():
     w = float(input("Enter the weight in lbs: "))
-    x = input("Are you a man or woman: ")
-    kg = w/2.2046
-    kg = round(kg,1)
+    x = get_gender()
+    kg = w / 2.2046
+    kg = round(kg, 1)
+
+    men_weight_classes = [
+        (52, "Flyweight"),
+        (57, "Featherweight"),
+        (63, "Lightweight"),
+        (69, "Welterweight"),
+        (75, "Middleweight"),
+        (81, "Light Heavyweight"),
+        (91, "Heavyweight"),
+        (float("inf"), "Super Heavyweight"),
+    ]
+
+    women_weight_classes = [
+        (51, "Flyweight"),
+        (57, "Featherweight"),
+        (60, "Lightweight"),
+        (69, "Welterweight"),
+        (75, "Middleweight"),
+    ]
 
     if x == "man":
         print("Olympic Men's boxing")
-        if kg <= 49:
-            print("kg:", kg)
-            print("Light Flyweight")
-        elif kg <= 52:
-            print("kg:", kg)
-            print("Flyweight")
-        elif kg <= 56:
-            print("kg:", kg)
-            print("Bantamweight")
-        elif kg <= 60:
-            print("kg:", kg)
-            print("Lightweight")
-        elif kg <= 64:
-            print("kg:", kg)
-            print("Light Welterweight")
-        elif kg <= 69:
-            print("kg:", kg)
-            print("Welterweight")
-        elif kg <= 75:
-            print("kg:", kg)
-            print("Middle weight")
-        elif kg <= 81:
-            print("kg:", kg)
-            print("Light Heavyweight")
-        elif kg <= 91:
-            print("kg:",kg)
-            print("Heavyweight")
-        else:
-            print("kg:", kg)
-            print("Super Heavyweight")
+        for weight, weight_class in men_weight_classes:
+            if kg <= weight:
+                print("kg:", kg)
+                print(weight_class)
+                break
 
     if x == "woman":
         print("Olympic Women's boxing")
-        if kg <= 48:
-            print("kg:",kg)
-            print("Light Flyweight")
-        elif kg <= 51:
-            print("kg:",kg)
-            print("Flyweight")
-        elif kg <= 54:
-            print("kg:",kg)
-            print("Bantamweight")
-        elif kg <= 57:
-            print("kg:",kg)
-            print("Featherweight")
-        elif kg <= 60:
-            print("kg:",kg)
-            print("Lightweight")
-        elif kg <= 64:
-            print("kg:",kg)
-            print("Welterweight")
-        elif kg <= 69:
-            print("kg:",kg)
-            print("Light Middleweight")
-        elif kg <= 75:
-            print("kg:",kg)
-            print("Middleweight")
-        elif kg <= 81:
-            print("kg:",kg)
-            print("Light Heavyweight")
-        else:
-            print("kg:",kg)
-            print("Heavyweight")
+
 
 Boxing()
